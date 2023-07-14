47877d536813fff1804d4bcf477c9c4e3874d715
Chad Bowler <chadbowler@gmail.com>
2023-07-14 17:01:41 -0600

    added get routes for products

--- models/index.js
+++ models/index.js
@@ -29,25 +29,6 @@ Tag.belongsToMany(Product, {
 });
 
 
-// Product.hasOne(Category, {
-//   foreignKey: 'driver_id',
-//   onDelete: 'CASCADE',
-// });
-// License.belongsTo(Driver, {
-//   foreignKey: 'driver_id',
-// });
-
-// // Define a Driver as having many Cars, thus creating a foreign key in the `car` table
-// Driver.hasMany(Car, {
-//   foreignKey: 'driver_id',
-//   onDelete: 'CASCADE',
-// });
-
-// // The association can also be created from the Car side
-// Car.belongsTo(Driver, {
-//   foreignKey: 'driver_id',
-// });
-
 module.exports = {
   Product,
   Category,
